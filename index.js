31e53905706a8788c875b31806c3062ecacce059
Ram <rammodi0509@gmail.com>
2023-08-16 16:19:14 +0530

    upgrade cors

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+
 const port =5000;
 
 
 const mongodb = require('./db');
 mongodb();
-app.use((req,res,next )=>{
-  res.setHeader("Access-Control-Allow-Origin","https://foodie-drab.vercel.app/")
-  res.header("Access-Control-Allow-Headers",
-  "Origin,X-Requested-Width,Content-Type, Accept"
-  )
-  next();
-})
+const corsOptions = {
+  origin: 'https://foodie-drab.vercel.app'
+};
+app.use(cors(corsOptions));
+// app.use((req,res,next )=>{
+//   res.setHeader("Access-Control-Allow-Origin","https://foodie-drab.vercel.app/")
+//   res.header("Access-Control-Allow-Headers",
+//   "Origin,X-Requested-Width,Content-Type, Accept"
+//   )
+//   next();
+// })
 app.use(express.json())  
 app.use('/api',require("./routes/createUser")) 
 app.use('/api',require("./routes/DisplayData")) 
